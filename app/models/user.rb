452fa6eec5db80cd1f3f44ca6ae3f679fec01e41
Tayyab <tayyab910629@gmail.com>
2023-07-11 18:42:06 +0100

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_many :transactions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+
+         devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :validatable
 
 end
--- db/migrate/20230711162716_add_confirmable_to_users.rb
+++ db/migrate/20230711162716_add_confirmable_to_users.rb
@@ -6,10 +6,3 @@ class AddConfirmableToUsers < ActiveRecord::Migration[6.1]
     add_column :users, :unconfirmed_email, :string
   end
 end
-
-
-## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
